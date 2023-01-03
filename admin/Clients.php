1e5ce2fe7589a1a6e16739bdacfaf3655903168f
9erch <34993177+9erch@users.noreply.github.com>
2023-01-03 17:44:43 +0100

    100

--- admin/Clients.php
+++ admin/Clients.php
@@ -178,7 +178,7 @@ $res = mysqli_num_rows($req);
                                                   <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                                                 </button>
                                             </div>
-                                            <div class="modal-body">
+                                            <div class="body" style="margin: 30px;">
                                             <div id='alerte'></div>
                                             <form class="row g-3" id='myforme'>
                                             <div class="col-md-6">
