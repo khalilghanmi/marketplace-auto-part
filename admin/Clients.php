83e629e1ca7c489a4661e5632586945a5cda0bc9
9erch <34993177+9erch@users.noreply.github.com>
2023-01-09 18:59:00 +0100

    985826

--- admin/Clients.php
+++ admin/Clients.php
@@ -67,68 +67,9 @@ $res = mysqli_num_rows($req);
                             </ol>
                         </nav>
                     </div>
-                    <div class="modal fade bd-example-modal-lg" id="empModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-lg" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    Modification
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                                    </button>
-                                                </div>
-                                                
-                                                    <div class="modal-body">
-                                                    
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <a href="#" class="btn btn-light-dark" data-bs-dismiss="modal">Annuler</a>
-                                                        <button type="button" class="btn btn-primary" id="modif" >Mise à jour</button>
-                                                        </form>
-                                                    </div>
-                                               
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="modal fade bd-example-modal-lg" id="empModale" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-lg" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    Suppression
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                                    </button>
-                                                </div>
-                                                
-                                                    <div class="modal-body-suprimer">
-                                                    
-                                                    </div>
-                                                    
-                                               
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="modal fade bd-example-modal-lg" id="empModalestatut" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-lg" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    Modification Statut
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                                    </button>
-                                                </div>
-                                                
-                                                    <div class="modal-body-statut">
-                                                    
-                                                    </div>
-                                                    
-                                               
-                                            </div>
-                                        </div>
-                                    </div>
-
-                     <!-- xtra Large modal -->
+                    <?php include 'controller/Clients/clients.php';   ?>
                      <button type="button" class="btn btn-primary mb-2 me-4" data-bs-toggle="modal" data-bs-target=".bd-example-modal-xl">Ajouter</button>
-                    <!-- Lage modal -->
+                    
                     <div class="row">
                  
                     <div class="row layout-top-spacing">
@@ -196,106 +137,12 @@ $res = mysqli_num_rows($req);
                     </div>
 
                 </div>
-
-            </div>
-            
-            <!--  BEGIN FOOTER  -->
-            <?php include 'head/footer.php';   ?>
-            <!--  END FOOTER  -->
-
+                                </div>
+            <?php include 'head/footer.php'; 
+            include 'controller/Clients/popup-ajouter.php';
+            ?>
         </div>
-        <!--  END CONTENT AREA  -->
     </div>
-    <!-- END MAIN CONTAINER -->
-    
-    <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-                                      <div class="modal-dialog modal-xl" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="myExtraLargeModalLabel">Ajouter Client</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                  <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                                </button>
-                                            </div>
-                                            <div class="body" style="margin: 30px;">
-                                            <div id='alerte'></div>
-                                            <form class="row g-3" id='myforme'>
-                                            <div class="col-md-6">
-                                            <label for="inputEmail4" class="form-label">Nom</label>
-                                            <input type="text" name="nom"class="form-control">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="inputPassword4" class="form-label">Prénom</label>
-                                            <input type="text" name="prenom" class="form-control">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="inputEmail4" class="form-label">Email</label>
-                                            <input type="email" name="email" class="form-control" id="inputEmail4">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="inputPassword4" class="form-label">Mot de passe</label>
-                                            <input type="password" name="password" class="form-control" id="inputPassword4">
-                                        </div>
-                                        <div class="col-12">
-                                            <label for="inputAddress" class="form-label">Addresse</label>
-                                            <input type="text" name="adresse" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="inputCity" class="form-label">Code Postale</label>
-                                            <input type="number" name="code_p" class="form-control" id="inputCity">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="inputState" class="form-label">Mobile</label>
-                                            <input type="phone"  name="mobile" class="form-control" id="inputCity">
-                                        </div>
-                                        <div class="col-md-6">
-                                        <label for="inputCivilité" class="form-label">Civilité</label><br>
-                                        <div class="form-check form-check-primary form-check-inline">
-                                            <input class="form-check-input" type="radio" value="homme" name="Civilite" id="form-check-radio-primary" checked="">
-                                             <label class="form-check-label" for="form-check-radio-primary">
-                                               Homme
-                                             </label>
-                                        </div>
-
-                                        <div class="form-check form-check-info form-check-inline">
-                                         <input class="form-check-input" type="radio" value="femme" name="Civilite" id="form-check-radio-info">
-                                             <label class="form-check-label" for="form-check-radio-info">
-                                                Femme
-                                             </label>
-                                        </div>
-                  
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="inputdatenessance" class="form-label">Date de naissance</label>
-                                            <input type="date" name="date_n" class="form-control">
-                                        </div>
-                                        
-                                        
-                                        <div class="col-8">
-                                            <div class="form-check">
-                                            <div class="form-check form-switch form-check-inline">
-                                            <input class="form-check-input" type="checkbox" name="statut" role="switch" id="flexSwitchCheckChecked" checked>
-                                            <label class="form-check-label" for="flexSwitchCheckChecked">Statut</label>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-2">
-                                        <a href="#" class="btn btn-light-dark" data-bs-dismiss="modal">Annuler</a>
-                                        </div>
-                                        <div class="col-2">          
-                                                                       
-                                        <a href="#" class="btn btn-primary" id='gi'>Enregistrer</a>
-                                        </div>
-                                    </form>
-
-                                            </div>
-                                            
-                                        </div>
-                                      </div>
-                    </div>
-    
-    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
-   <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
    <script src="../src/plugins/src/global/vendors.min.js"></script>
     <script src="../src/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
@@ -344,8 +191,6 @@ $res = mysqli_num_rows($req);
         });
         multiCheck(ecommerceList);
     </script>
-    <!-- END PAGE LEVEL SCRIPTS -->    
-
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/plugins.js"></script>
 <script type="text/javascript" src="js/jquery.min.js"></script>
---
+++ admin/controller/Clients/clients.php
@@ -0,0 +1,58 @@
+<div class="modal fade bd-example-modal-lg" id="empModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    Modification
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                    </button>
+                                                </div>
+                                                
+                                                    <div class="modal-body">
+                                                    
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <a href="#" class="btn btn-light-dark" data-bs-dismiss="modal">Annuler</a>
+                                                        <button type="button" class="btn btn-primary" id="modif" >Mise à jour</button>
+                                                        </form>
+                                                    </div>
+                                               
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal fade bd-example-modal-lg" id="empModale" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    Suppression
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                    </button>
+                                                </div>
+                                                
+                                                    <div class="modal-body-suprimer">
+                                                    
+                                                    </div>
+                                                    
+                                               
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal fade bd-example-modal-lg" id="empModalestatut" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    Modification Statut
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                    </button>
+                                                </div>
+                                                
+                                                    <div class="modal-body-statut">
+                                                    
+                                                    </div>
+                                                    
+                                               
+                                            </div>
+                                        </div>
+                                    </div>
