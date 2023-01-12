ee52a4365f473f8eb3c7518ac1e65920d9a87555
9erch <34993177+9erch@users.noreply.github.com>
2023-01-12 20:21:59 +0100

    95962

---
+++ Untitled-1.php
@@ -0,0 +1,23 @@
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="myLargeModalLabel">Large</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                   
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button class="btn btn-light-dark" data-bs-dismiss="modal">Discard</button>
+                                                    <button type="button" class="btn btn-primary">Save</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="alert alert-light-danger alert-dismissible fade show border-0 mb-4" role="alert">
+                                    Confirmez-vous vouloir supprimer SVG Support et ses données ? </button></div>
