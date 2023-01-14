5f7e24f7020ae772cb1ee40231dd7bf5cd31b413
9erch <34993177+9erch@users.noreply.github.com>
2023-01-14 03:41:45 +0100

    555

--- admin/Liste-des-dategories-produits.php
+++ admin/Liste-des-dategories-produits.php
@@ -114,7 +114,62 @@ $res = mysqli_num_rows($req);
                                     </td>
                                       </tr>
                                     ';
-                                    
+                                    $concat_name=$data['name_category'];
+
+
+                                    $idcat=$data['category_id'];
+$sql = "SELECT * FROM category";
+$reqs = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+$reso = mysqli_num_rows($reqs);
+if($reso!=0)  // l'url existe déjà, on affiche un message d'erreur
+{
+    While ($datacat = mysqli_fetch_array($reqs)){
+        $sql = "SELECT * FROM category,category_description WHERE parent_id = '$idcat' and category.category_id = category_description.category_id";
+        $reqs = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+        $reso = mysqli_num_rows($reqs);
+        if($reso!=0)  // l'url existe déjà, on affiche un message d'erreur
+        {
+        $datas = mysqli_fetch_array($reqs);
+        echo'<tr>
+        <td class="text-center"> '.$datas['category_id'].'</td>
+        <td class="text-center"> 
+                        <div class="avatar  me-3">
+                            <img src="../'.$datas['category_image'].'" alt="Avatar" width="64" height="64">
+                        </div>
+                    </td>';
+                    $concat_name.=' > '.$datas['name_category'].'';
+                        echo'<td class="text-center"> '.$concat_name.'</td>';
+                        
+            
+        if($data['Statut'] == 1){
+            echo'<td class="text-center"><span class="shadow-none badge badge-primary">activé</span></td>';
+        }
+        else{
+            echo'<td class="text-center"><span class="shadow-none badge badge-danger">désactivé</span></td>';
+        }
+        echo'<td class="text-center">
+        <div class="btn-group">
+            <a class="btn btn-dark btn-sm userinfo " data-bs-target=".bd-example-modal-lg" data-id="'.$datas['category_id'].'" href="#">Modifier</a>
+            <button type="button" class="btn btn-dark btn-sm dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuReference1">
+                <a class="dropdown-item userinfos" data-bs-target=".bd-example-modal-lg" data-id="'.$datas['category_id'].'" href="#">Supprimer</a>
+                <a class="dropdown-item userinfosstatut" data-id="'.$datas['category_id'].'" href="#">Statut</a>
+                
+            </div>
+        </div>
+        </td>
+          </tr>
+        ';
+        $idcat=$datas['category_id'];
+    }    
+}                             
+
+
+}
+
+
                                     }
                                 }
                                     ?>
