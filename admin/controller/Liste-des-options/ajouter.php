890aad0b1d1a4b3f34e8146c0f174d735e4d01e9
9erch <34993177+9erch@users.noreply.github.com>
2023-01-27 21:22:37 +0100

    95485426

--- admin/controller/Liste-des-options/ajouter.php
+++ admin/controller/Liste-des-options/ajouter.php
@@ -1,28 +1,74 @@
 <?php
 include '../connection.php'; 
- 
-  $date_added=date('y-m-d');
- 
-		
-		$sql = "SELECT * FROM customers WHERE email='$email' or mobile='$mobile'";
-		$req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
-		
-		$res = mysqli_num_rows($req);
-		if($res!=0)  
-		{
-		echo '<div class="alert alert-light-danger alert-dismissible fade show border-0 mb-4" role="alert">
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
-    Désolé, email ou mobile existe déjà dans site web</button>
-</div>';
-		}
-		else  
-		{
-      $today = date("y-m-d");
-      $password = md5($password);
-		$sql = "INSERT  INTO customers VALUES ( '', '$nom', '$prenom', '$email', '$Civilite', '$date_n', '$adresse', '$code_p', '$mobile', '$statut', '$password', '' , '$today')";
-		$req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
-		echo "";
-		}
- 
+if(isset($_POST['Statut'])) {
+    $statut=1; }
+    else{
+    $statut=0;
+    }  
+
+$date_added=date('y-m-d');
+$name_options=$_POST['name_options'];
+$type_options=$_POST['type_options'];
+
+
+$date_added=date('y-m-d');
+  			$sql = "INSERT  INTO options VALUES ( '', '0', '$name_options','$type_options','','','$statut')";
+  			$req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());			 
+		    if($type_options=='Select' || $type_options=='Radio' || $type_options=='Checkbox'){
+				$sql = "SELECT * FROM options WHERE name_options='$name_options'";
+				$req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+				$data = mysqli_fetch_array($req);
+				$name_value_options=$_POST['name_value_options'];
+				$id_values = $data['id_options'];
+			 $imgFile = $_FILES['image_options']['name'];
+             $tmp_dir = $_FILES['image_options']['tmp_name'];
+             $imgSize = $_FILES['image_options']['size'];
+            if(empty($imgFile)){
+                
+                $name_image_finales='image/options/defaults.png';
+                $sql = "INSERT  INTO options VALUES ( '', '$id_values', '$name_options','$type_options','$name_value_options','$name_image_finales','$statut')";
+                $req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+                
+                header("refresh:0;../../Liste-des-dategories-produits.php");
+            }
+            else
+            { $upload_dir = '../../../image/categories/';
+                
+                $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); // get image extension
+             
+                $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'webp', 'svg'); // valid extensions
+                
+                //  $userpic = $khalilovic.rand(1,10000).".".$imgExt;
+                 $userpic = $name_category.'_'.uniqid().".".$imgExt;
+                    
+                // allow valid image file formats
+                if(in_array($imgExt, $valid_extensions)){			
+                    // Check file size '5MB'
+                    if($imgSize < 500000000000000)				{
+                        move_uploaded_file($tmp_dir,$upload_dir.$userpic);
+                    }
+                    else{
+                        $errMSG = "Sorry, your file is too large.";
+                    }
+                }
+                else{
+                    $errMSG = "Sorry, only JPG, JPEG, PNG , webp & GIF files are allowed.";		
+                }
+                $name_images='';		
+                $name_images.='image/categories/';
+                $name_images.=$userpic;
+                
+                if(!isset($errMSG))
+                {
+                $sql = "INSERT  INTO category VALUES ( '', '0', '$name_category','$description_category','$name_images','$meta_title','$meta_description','$meta_keyword','$statut','$date_added')";
+                $req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+                
+                header("refresh:0;../../Liste-des-dategories-produits.php");  
+             
+                }
+            }
+			}
+			
+
      
 ?>
--- admin/controller/Liste-des-options/controller-ajouter.php
+++ admin/controller/Liste-des-options/controller-ajouter.php
@@ -1,6 +1,6 @@
 <?php
 include '../connection.php'; 
-if(empty($name_options))
+if(empty($name_options) or empty($type_options) )
     {
     echo '<div class="alert alert-light-danger alert-dismissible fade show border-0 mb-4" role="alert">
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
@@ -8,6 +8,17 @@ if(empty($name_options))
 </div> 
    ';
     }
- 
-  
+else{
+    $sql = "SELECT * FROM options WHERE name_options='$name_options'";
+			$req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+		
+			$res = mysqli_num_rows($req);
+			if($res!=0)  
+			{
+			echo '<div class="alert alert-light-danger alert-dismissible fade show border-0 mb-4" role="alert">
+    	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
+    	Désolé, name options existe déjà dans site web</button>
+				</div>';
+			}
+}
 ?>
