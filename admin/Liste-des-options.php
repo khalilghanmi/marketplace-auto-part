6971d731f10f773b511722ac1b63d35c618c5367
9erch <34993177+9erch@users.noreply.github.com>
2023-01-29 18:52:06 +0100

    85444

--- admin/Liste-des-options.php
+++ admin/Liste-des-options.php
@@ -1,6 +1,6 @@
 <?php
 include 'controller/connection.php';             
-$sql = "SELECT * FROM marques";
+$sql = "SELECT * FROM options WHERE id_value_options='0'";
 $req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
 $res = mysqli_num_rows($req);
 ?>
@@ -65,7 +65,8 @@ $res = mysqli_num_rows($req);
                                     <thead>
                                         <tr>
                                             <th class="text-center">Id</th>
-                                            <th class="text-center">Nom marques</th>
+                                            <th class="text-center">Nom options</th>
+                                            <th class="text-center">Type Options</th>
                                             <th class="text-center">Statut</th>
                                             <th class="text-center">Action</th>
                                         </tr>
@@ -77,9 +78,9 @@ $res = mysqli_num_rows($req);
                                     While ($data = mysqli_fetch_array($req)){
                                     
                                     echo'<tr>
-                                    <td class="text-center"> '.$data['id_marques'].'</td>
-                                    <td class="text-center"> '.$data['name_marques'].'</td>
-                                    
+                                    <td class="text-center"> '.$data['id_options'].'</td>
+                                    <td class="text-center"> '.$data['name_options'].'</td>
+                                    <td class="text-center"> '.$data['type_options'].'</td>
                                     ';
                                     if($data['Statut'] == 1){
                                         echo'<td class="text-center"><span class="shadow-none badge badge-primary">activé</span></td>';
@@ -89,13 +90,13 @@ $res = mysqli_num_rows($req);
                                     }
                                     echo'<td class="text-center">
                                     <div class="btn-group">
-                                        <a class="btn btn-dark btn-sm userinfo " data-bs-target=".bd-example-modal-lg" data-id="'.$data['id_marques'].'" href="#">Modifier</a>
+                                        <a class="btn btn-dark btn-sm userinfo " data-bs-target=".bd-example-modal-lg" data-id="'.$data['id_options'].'" href="#">Modifier</a>
                                         <button type="button" class="btn btn-dark btn-sm dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                                         </button>
                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuReference1">
-                                            <a class="dropdown-item userinfos" data-bs-target=".bd-example-modal-lg" data-id="'.$data['id_marques'].'" href="#">Supprimer</a>
-                                            <a class="dropdown-item userinfosstatut" data-id="'.$data['id_marques'].'" href="#">Statut</a>
+                                            <a class="dropdown-item userinfos" data-bs-target=".bd-example-modal-lg" data-id="'.$data['id_options'].'" href="#">Supprimer</a>
+                                            <a class="dropdown-item userinfosstatut" data-id="'.$data['id_options'].'" href="#">Statut</a>
                                             
                                         </div>
                                     </div>
