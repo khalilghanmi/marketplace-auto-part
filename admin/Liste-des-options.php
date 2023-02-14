a7dea833246350d250ec73e0bf7d229da0f476c1
9erch <34993177+9erch@users.noreply.github.com>
2023-02-14 20:16:40 +0100

    9845826

--- admin/Liste-des-options.php
+++ admin/Liste-des-options.php
@@ -90,7 +90,7 @@ $res = mysqli_num_rows($req);
                                     }
                                     echo'<td class="text-center">
                                     <div class="btn-group">
-                                        <a class="btn btn-dark btn-sm userinfo " data-bs-target=".bd-example-modal-lg" data-id="'.$data['id_options'].'" href="#">Modifier</a>
+                                        <button class="btn btn-dark btn-sm userinfo " data-bs-target=".bd-example-modal-lg" data-id="'.$data['id_options'].'" >Modifier</button>
                                         <button type="button" class="btn btn-dark btn-sm dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                                         </button>
