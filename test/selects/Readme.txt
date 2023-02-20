034b562b43b3cbd363f3fe90b03dcf8b6889ff9d
9erch <34993177+9erch@users.noreply.github.com>
2023-02-20 16:52:40 +0100

    695296

--- test/selects/Readme.txt
+++
@@ -1,4 +0,0 @@
-Author: Yogesh singh
-Author URL: https://makitweb.com/
-Author Email: yogesh@makitweb.com
-Tutorial Link: https://makitweb.com/change-selected-option-in-select2-dropdown-with-jquery/
--- test/tableau-add/affichage.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'root', '','test');
-$nom=$_POST['nom'];
-$sql = "INSERT  INTO test_name VALUES ( '', '$nom')";
-$req = mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
-echo'insertions succées';
-?>
